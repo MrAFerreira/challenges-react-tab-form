3a4bb06fea3820998012d3b920463ba5b6f45bc2
André Ferreira <mrawesomeferreira@gmail.com>
2020-04-20 16:24:03 +0100

    minor fix

--- src/components/Tabform/TabForm.js
+++ src/components/Tabform/TabForm.js
@@ -38,7 +38,9 @@ class TabForm extends Component {
               After their first OfferTest purchase, they win 10.000 extra free credits each and
               you'll get 10.000 free credits from each friend.
             </p>
-            <p>Remember you have only 2 invitations, use them wisely!</p>
+            <p>
+              <strong>Remember you have only 2 invitations, use them wisely!</strong>
+            </p>
           </div>
         </Carousel>
       </div>
