3c0be042f32f3e07231e76d05bcd59976b0cd772
Andre Ferreira <mrawesomeferreira@gmail.com>
2020-04-20 15:14:32 +0100

    added antd grid

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Row, Col } from 'antd';
 import TabForm from './components/Tabform/TabForm';
 import FormInputs from './components/FormInputs/FormInputs';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <TabForm className="tabform" />
-      <FormInputs className="forminputs" />
-    </div>
+    <>
+      <Row type="flex" justify="center" align="middle">
+        <Col sm={24} md={12}>
+          <TabForm className="tabform" />
+        </Col>
+        <Col sm={24} md={12}>
+          <FormInputs className="forminputs" />
+        </Col>
+      </Row>
+    </>
   );
 }
 
